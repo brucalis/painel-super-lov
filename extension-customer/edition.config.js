e6ec407542cd84569a229a0a35fdb03558bfe64a
brucalis <144153810+brucalis@users.noreply.github.com>
2026-09-03 18:21:07 -0300

    release: atualizar identificação da edição comercial

--- extension-customer/edition.config.js
+++ extension-customer/edition.config.js
@@ -1,6 +1,6 @@
 globalThis.SUPER_LOVABLE_EDITION = Object.freeze({
   mode: "customer",
   id: "customer-s1",
-  publicVersion: "03.09.S1",
-  technicalVersion: "33.0.1",
+  publicVersion: "03.09.S2",
+  technicalVersion: "33.0.2",
 });
