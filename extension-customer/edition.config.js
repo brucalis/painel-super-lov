2be4e7cf07a58822a24c923b14705d020ec7c5c1
brucalis <144153810+brucalis@users.noreply.github.com>
2026-09-03 20:24:57 -0300

    release: preparar versão comercial 03.09.S4

--- extension-customer/edition.config.js
+++ extension-customer/edition.config.js
@@ -1,6 +1,6 @@
 globalThis.SUPER_LOVABLE_EDITION = Object.freeze({
   mode: "customer",
   id: "customer-s1",
-  publicVersion: "03.09.S3",
-  technicalVersion: "33.0.3",
+  publicVersion: "03.09.S4",
+  technicalVersion: "33.0.4",
 });
