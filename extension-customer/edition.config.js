b958c399af576a8c1b8686929c545d6fd22a8d92
brucalis <144153810+brucalis@users.noreply.github.com>
2026-09-04 11:16:58 -0300

    chore: atualizar edição comercial para 04.09.S3

--- extension-customer/edition.config.js
+++ extension-customer/edition.config.js
@@ -1,6 +1,6 @@
 globalThis.SUPER_LOVABLE_EDITION = Object.freeze({
   mode: "customer",
   id: "customer-s1",
-  publicVersion: "04.09.S2",
-  technicalVersion: "33.0.6",
+  publicVersion: "04.09.S3",
+  technicalVersion: "33.0.7",
 });
