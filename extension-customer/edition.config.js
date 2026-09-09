a7a55e60938f0700f9d0789e1aa2588b51f0f44c
brucalis <144153810+brucalis@users.noreply.github.com>
2026-09-08 23:27:18 -0300

    release: preparar Super Lovable 08.09.S3

--- extension-customer/edition.config.js
+++ extension-customer/edition.config.js
@@ -1,6 +1,6 @@
 globalThis.SUPER_LOVABLE_EDITION = Object.freeze({
   mode: "customer",
   id: "customer-s1",
-  publicVersion: "08.09.S2",
-  technicalVersion: "33.0.11",
+  publicVersion: "08.09.S3",
+  technicalVersion: "33.0.12",
 });
